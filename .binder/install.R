412db95530eabfcff2c1479018ccda000ef1ba32
Kevin Donkers <kevin.donkers@metoffice.gov.uk>
2023-04-12 08:48:37 +0100

    Minimum R env
    Add minimum packages required to run R notebook using install.R file in .binder/, as a backup to using renv

--- .binder/install.R
+++ .binder/install.R
@@ -1,2 +1,9 @@
 # Install R packages at build time install.packages() syntax
 
+#install.packages("Boruta")
+#install.packages("ggplot2")
+#install.packages("IRdisplay")
+#install.packages("bnlearn")
+#install.packages("igraph")
+#install.packages("compare")
+#install.packages("boot")
