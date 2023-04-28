0fc5bdc18fbe123edcd8ea5b0ab665d0da1f3601
Nathan Creaser <72136844+nathan962@users.noreply.github.com>
2023-04-28 17:33:13 +0100

    R notebook (#10)
    Working notebook containing all techniques used in the paper (except for dynamics plot)

--- .binder/install.R
+++ .binder/install.R
@@ -1,2 +1,12 @@
 # Install R packages at build time install.packages() syntax
 
+install.packages("Boruta")
+install.packages("ggplot2")
+install.packages("IRdisplay")
+install.packages("bnlearn")
+install.packages("igraph")
+install.packages("compare")
+install.packages("repr")
+install.packages("patchwork")
+install.packages("randomForest")
+install.packages("boot")
